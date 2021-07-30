6df75f27c18ec49e95380f065dcfc710daf889aa
Dogukan <dogukangulyasar@gmail.com>
2021-07-30 15:50:35 +0300

    Connection script changed

--- br2-external-ftdev/board/tk1/rootfs_overlay/root/connection.sh
+++ br2-external-ftdev/board/tk1/rootfs_overlay/root/connection.sh
@@ -5,8 +5,7 @@ if [ $1 == 'connect' ]
 then
 	echo '############################## Connection process started'
 	connmanctl enable wifi
-	dbus-send --system --dest=net.connman --print-reply /
-	net.connman.Manager.RegisterAgent objpath:"/test/agent"
+	#Not sure it will work -> Agent registration. dbus-send --system --dest=net.connman --print-reply / net.connman.Manager.RegisterAgent objpath:"/test/agent"
 	connmanctl scan wifi
 	connmanctl connect wifi_0013eff3084c_47554e53454c5f5445414d_managed_psk
 	echo '############################## Connection process ended'	
